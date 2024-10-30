2c4720b99b6c0a14b6d986004707f18f77ed9766
Alberto <dany.canaviri2022@gmail.com>
2024-10-29 22:48:26 -0400

    Effect scroll up completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,9 +54,17 @@ const swiperServices = new Swiper('.services__swiper', {
 
 });
   
-
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
 
+window.addEventListener('scroll', scrollUp)
+
+scrollUp()
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
